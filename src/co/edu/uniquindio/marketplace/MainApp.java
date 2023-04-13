ef0f03b463b32f651487943e8df4b5d9a4fb4937
carlos-a-pv <carlosa.perezv@uqvirtual.edu.co>
2023-04-13 12:59:01 -0500

    interfaz inicio de sesion, con el main app

---
+++ src/co/edu/uniquindio/marketplace/MainApp.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.marketplace;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/co/edu/uniquindio/marketplace/views/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
