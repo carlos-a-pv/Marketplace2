697bd7aabb224e232f5f4eb239aa9d7fbf026b02
carlos-a-pv <carlosa.perezv@uqvirtual.edu.co>
2023-04-16 13:10:30 -0500

    diseño de la interface administrador terminada

--- src/co/edu/uniquindio/marketplace/MainApp.java
+++ src/co/edu/uniquindio/marketplace/MainApp.java
@@ -1,17 +1,21 @@
 package co.edu.uniquindio.marketplace;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainApp extends Application {
+    @FXML
+    
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/co/edu/uniquindio/marketplace/views/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/co/edu/uniquindio/marketplace/views/administrador-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
--- src/co/edu/uniquindio/marketplace/controllers/AdministradorViewController.java
+++ src/co/edu/uniquindio/marketplace/controllers/AdministradorViewController.java
@@ -1,4 +1,31 @@
 package co.edu.uniquindio.marketplace.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class AdministradorViewController {
+    ModelFactoryController modelFactoryController;
+    @FXML
+    private Button btnVolver;
+    @FXML
+    void initialize() {
+        Image img = new Image("/resources/hacia-atras.png");
+        ImageView view = new ImageView(img);
+        view.setFitHeight(20);
+        view.setPreserveRatio(true);
+        btnVolver.setGraphic(view);
+        modelFactoryController = ModelFactoryController.getInstance();
+    }
+
+    public void OnVolverClick(ActionEvent actionEvent) {
+    }
+
+    public void onCrearVendedorClick(ActionEvent actionEvent) {
+    }
+
+    public void onBuscarClick(ActionEvent actionEvent) {
+    }
 }
